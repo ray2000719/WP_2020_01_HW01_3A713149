38a58ae54a1df95236b94cffd526c5c87569c564
ray2000719v414307@gmail.com <ray2000719v414307@gmail.com>
2020-11-12 18:35:17 +0800

    fix

--- WP_2020_01_HW01_3A713149/Form1.cs
+++ WP_2020_01_HW01_3A713149/Form1.cs
@@ -24,24 +24,24 @@ namespace WP_2020_01_HW01_3A713149
         private void button1_Click(object sender, EventArgs e)
         {
 
-            var cryptoMD5 = System.Security.Cryptography.MD5.Create();
-            byte[] source = Encoding.Default.GetBytes(tbinput.Text);//將字串轉為Byte[]
-            byte[] crypto = cryptoMD5.ComputeHash(source);//進行MD5加密
+            //var cryptoMD5 = System.Security.Cryptography.MD5.Create();
+            //byte[] source = Encoding.Default.GetBytes(tbinput.Text);//將字串轉為Byte[]
+            //byte[] crypto = cryptoMD5.ComputeHash(source);//進行MD5加密
             //string result = Convert.ToBase64String(source);
-            var result = BitConverter.ToString(crypto).Replace("-", String.Empty).ToUpper();
+            //var result = BitConverter.ToString(crypto).Replace("-", String.Empty).ToUpper();
 
 
 
 
 
-            //var str = tbinput.Text;
-            //var md5 = str.ToMD5();
-            rtboutput.Text = result;
+            var str = tbinput.Text;
+            var md5 = str.ToMD5();
+            rtboutput.Text = md5;
 
 
         }
     }
-    /*public static class MD5Extensions
+    public static class MD5Extensions
     {
         public static string ToMD5(this string str)
         {
@@ -62,5 +62,5 @@ namespace WP_2020_01_HW01_3A713149
                 return md5;
             }
         }
-    }*/
+    }
 }
